4a2cc8824b283521a50aa21a68a6c9110a638d20
RomanKhorin <rhorin@yandex.ru>
2016-05-02 14:19:05 +0300

    First Test Commit

--- AutoCenter/MainWindow.xaml.cs
+++ AutoCenter/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,27 @@ namespace AutoCenter
     /// </summary>
     public partial class MainWindow : Window
     {
+        //private SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\v11.0;AttachDbFilename=" + System.IO.Path.GetFullPath("../../DB.mdf") + ";Integrated Security=True;Connect Timeout=30");
+        private SqlConnection connection = new SqlConnection("Data Source=ROMAPC\\SQLEXPRESS;Initial Catalog=Car_Center;Integrated Security=True");
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void test_button_Click(object sender, RoutedEventArgs e)
+        {
+            test_listbox.Items.Clear();
+            connection.Open();
+            var rental_cars = new SqlCommand(("select Firm, Model, Car_Number as [Car number], Colour, Engine, Country, Description"+
+                " from Rental_Car rc inner join Condition c on rc.Condition_Number = c.Condition_Number"), connection);
+            SqlDataReader reader = rental_cars.ExecuteReader();
+            while (reader.Read())
+                test_listbox.Items.Add(reader.GetString(0) + " " + reader.GetString(1) + " | " + reader.GetString(2));
+
+            connection.Close();
+        
+        
         }
     }
 }
